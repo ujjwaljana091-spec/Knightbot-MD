71564faea1993b2bbd7adee0932689b50ed5aae9
ujjwaljana091-spec <ujjwaljana091@gmail.com>
2026-09-21 12:43:44 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'Knight Bot',
   author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botName: "Mario Bot",
+  botOwner: 'Mario', // Your name
+  ownerNumber: '918653279303', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
